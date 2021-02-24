9f4a598d54c6e0b9fa93d3ed04fcb2447dbba510
steffkes <stefan@gelungen.es>
2021-02-24 08:41:51 +0100

    image generator

--- bin/image-generator.py
+++ bin/image-generator.py
@@ -1,3 +1,14 @@
+import frontmatter
+import datetime
+from urllib.parse import urlparse
+
+date = "2021-02-24"
+
+with open("content/posts/%s.md" % date) as f:
+    post = frontmatter.load(f)
+
+print(f"""
+#!/bin/sh
 time convert \
     -interline-spacing -20 \
     -border 20 \
@@ -7,14 +18,15 @@ time convert \
     -size 760x560 \
     -gravity west \
     -font fonts/bevan.ttf \
-    caption:"Man(n) gerät immer wieder in Situationen, wo es auf zuverlässigen Halt ankommt." \
-    static/images/2021-02-19.png;
+    caption:"{post["title"].replace('"','""')}" \
+    static/images/{date}.png;
 time convert \
-    static/images/2021-02-19.png \
+    static/images/{date}.png \
     -font fonts/pt-sans.ttf \
     -pointsize 16 \
     -fill black -gravity SouthWest -annotate +43+5 "Gelungenes" \
     -fill grey -gravity SouthWest -annotate +128+5 "Die Kunst des schönen Satzes" \
-    -fill grey -gravity SouthEast -annotate +20+5 "gard.de @ 2014-12-09" \
+    -fill grey -gravity SouthEast -annotate +20+5 "{urlparse(post["found_url"]).netloc.lstrip("www.")} @ {post["found_at"]}" \
     static/favicon.ico -gravity SouthWest -geometry +20+8 -composite  \
-    static/images/2021-02-19.png
+    static/images/{date}.png
+""")
